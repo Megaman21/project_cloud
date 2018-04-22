44e1e9688d689caa527a8f899872cbf54e79a42a
Megaman21 <haseeb.ahmed21@gmail.com>
2018-04-22 19:49:36 +0500

    server deploys

--- megamaninn/forms.py
+++ megamaninn/forms.py
@@ -4,9 +4,7 @@ from megamaninn.models import Customer
 
 
 class SignUpForm(UserCreationForm):
-    name = forms.CharField(max_length=50, required=True, help_text='Optional.')
-    password = forms.CharField(max_length=50, required=True, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    
 
     class Meta:
         model = Customer
