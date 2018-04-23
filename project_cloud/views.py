44ef3ce4f3f11ef61b81ca6cf097d5e4b9167bbf
Megaman21 <haseeb.ahmed21@gmail.com>
2018-04-23 17:03:20 +0500

    sign up

--- project_cloud/views.py
+++ project_cloud/views.py
@@ -4,84 +4,104 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import loader
 from megamaninn.forms import SignUpForm
+
+
 def index(request):
-    template=loader.get_template('test.html')
-    context= {
+    template = loader.get_template('test.html')
+    context = {
 
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
 
 def loginpage(request):
-    template=loader.get_template('login.html')
-    context= {
+    if request.method == "GET":
+        template = loader.get_template('login.html')
+        context = {
 
-    }
+        }
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if user:
+            login(request, user)
+            return render(request, 'homepage.html')
+        else:
+            return render(request,'login.html')
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
 
 def home(request):
-    template=loader.get_template('homepage.html')
-    context= {
+    template = loader.get_template('homepage.html')
+    context = {
 
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
 
 def editpage(request):
-    template=loader.get_template('edit_profile.html')
-    context= {
+    template = loader.get_template('edit_profile.html')
+    context = {
 
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
 
 
 def signuppage(request):
     if request.method == "GET":
-     template=loader.get_template('signup.html')
-     context= {
+        template = loader.get_template('signup.html')
+        context = {
 
-     }
+        }
 
-     return HttpResponse(template.render(context,request))
+        return HttpResponse(template.render(context, request))
     if request.method == "POST":
-        name=request.POST['name']
-        email=request.POST['email']
-        password=request.POST['password']
-        username=email
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        username = email
 
-        firstname=name.strip().split(' ')[0]
-        lastname=' '.join((name+' ').split(' ')[1:]).strip()
+        firstname = name.strip().split(' ')[0]
+        lastname = ' '.join((name + ' ').split(' ')[1:]).strip()
 
         if username and password:
-            user, created=User.objects.get_or_create(username=username,email=email,first_name=firstname,last_name=lastname)
+            user, created = User.objects.get_or_create(username=username, email=email, first_name=firstname,
+                                                       last_name=lastname)
 
             if created:
                 user.set_password(password)
                 user.save()
 
-            user=authenticate(username=username, password=password)
-            login(request,user)
+            user = authenticate(username=username, password=password)
+            login(request, user)
 
-            return render(request,'homepage.html')
+            return render(request, 'homepage.html')
     else:
-        return render(request,'signup.html')
+        return render(request, 'signup.html')
+
 
 def bookpage(request):
-    template=loader.get_template('booking.html')
-    context= {
+    template = loader.get_template('booking.html')
+    context = {
 
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
+
 def reviewpage(request):
-    template=loader.get_template('review.html')
-    context= {
+    template = loader.get_template('review.html')
+    context = {
 
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
 
 def doregister(request):
     if request.method == "POST":
